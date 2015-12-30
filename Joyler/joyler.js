d6afbd3c2a9ad6c6b81851d981b9869c56873cd7
Jess <jkpenick@gmail.com>
2015-12-30 18:51:38 -0500

    adds js projs n html shell

--- Joyler/joyler.js
+++ Joyler/joyler.js
@@ -1,3 +1,16 @@
+window.onload = function(){
+	//init globals
+	var p1Box = document.getElementById("p1Box");
+	var p1Butt = document.getElementById("p1Butt");
+
+	//add onclick function
+	p1Butt.onclick = function(){
+		p1result = prob1();
+		p1Box.innerHTML += p1result;
+	}
+}
+
+
 /* Problem 1
 If we list all the natural numbers below 10 that are multiples
  of 3 or 5, we get 3, 5, 6 and 9. 
@@ -5,12 +18,13 @@ If we list all the natural numbers below 10 that are multiples
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 */
-var prob1 = new function(){
+var prob1 = function(){
 	var sum = 0;
 	for (var i=0; i<=1000; i++) {
 		if (i%3==0 || i%5==0) {
 			sum+=i;
 		};
 	};
+	//console.log(sum);
 	return sum;
 }
