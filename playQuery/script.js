7a4280a6aec156d3b3373bd47a33b23abaa2381b
Jess <jkpenick@gmail.com>
2016-01-18 18:46:26 -0500

    adds hover effect

--- playQuery/script.js
+++ playQuery/script.js
@@ -1,10 +1,16 @@
 $(document).ready(function(){
 	var $gB = $('#gridBox');
+
 	//16x16 grid divs, new lines inline-block
 	for (var i=0; i<16; i++) {
+		//jQuery scrubs such as I must build from scratch
+		$gB.append('<div class="gridRow" id="gR'+i+'"></div>')
 		for (var j=0; j<16; j++) {
-			$gB.append('<div class="gridDiv">'+j+','+i+'</div>');
+			$('#gR'+i).append('<div class="gridDiv"></div>');
 		};
-		$gB.append('<br>')
 	};
+	
+	$('.gridDiv').mouseenter(function() {
+		$(this).addClass('filled');
+	});
 });
