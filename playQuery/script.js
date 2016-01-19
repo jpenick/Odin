865e30bf6f0859fbda25ecd757b9168162b5427b
Jess <jkpenick@gmail.com>
2016-01-18 23:35:12 -0500

    delegates events

--- playQuery/script.js
+++ playQuery/script.js
@@ -1,13 +1,18 @@
 $(document).ready(function(){
 	var gB = $('#gridBox');
 
-	
+	/* You've got to delegate the event to someone
+		who'll stick around. The .gridDivs all 
+		get destroyed by the button, but the gridBox
+		marches on, so gB is the keeper of the event
+		for the .gridDivs.
+	*/
 	// draw stuff
-	$('.gridDiv').mouseenter(function() {
+	gB.on('mouseenter', '.gridDiv', function() {
 		$(this).addClass('filled');
 	});
 
-	//make a cool grid
+	// make a cool grid
 	var newGrid = function(n){
 		for (var i=0; i<n; i++) {
 			//jQuery scrubs such as I must build from scratch
@@ -19,7 +24,7 @@ $(document).ready(function(){
 	}
 	newGrid(16);
 
-	//shake the etch-a-sketch
+	// shake the etch-a-sketch (erase, prompt for resizing)
 	$('button').click(function() {
 		gB.empty();
 		var n = prompt("How many square pixels?");
