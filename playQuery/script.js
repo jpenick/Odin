056060a178d7fc55d1c5bef35797ba77c7a282f1
Jess <jkpenick@gmail.com>
2016-01-18 21:40:19 -0500

    perfects grid dimensions

--- playQuery/script.js
+++ playQuery/script.js
@@ -1,14 +1,16 @@
 $(document).ready(function(){
 	var $gB = $('#gridBox');
-
 	//16x16 grid divs, new lines inline-block
-	for (var i=0; i<16; i++) {
-		//jQuery scrubs such as I must build from scratch
-		$gB.append('<div class="gridRow" id="gR'+i+'"></div>')
-		for (var j=0; j<16; j++) {
-			$('#gR'+i).append('<div class="gridDiv"></div>');
+	var newGrid = function(r,c){
+		for (var i=0; i<c; i++) {
+			//jQuery scrubs such as I must build from scratch
+			$gB.append('<div class="gridRow" id="gR'+i+'"></div>')
+			for (var j=0; j<r; j++) {
+				$('#gR'+i).append('<div class="gridDiv"></div>');
+			};
 		};
-	};
+	}
+	newGrid(16,16)
 	
 	$('.gridDiv').mouseenter(function() {
 		$(this).addClass('filled');
