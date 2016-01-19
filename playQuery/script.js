f9ef8465382061efcd9d1660fc9a8d9617e7890f
Jess <jkpenick@gmail.com>
2016-01-18 22:42:23 -0500

    adds new grid button

--- playQuery/script.js
+++ playQuery/script.js
@@ -1,18 +1,29 @@
 $(document).ready(function(){
-	var $gB = $('#gridBox');
-	//16x16 grid divs, new lines inline-block
-	var newGrid = function(r,c){
-		for (var i=0; i<c; i++) {
+	var gB = $('#gridBox');
+
+	
+	// draw stuff
+	$('.gridDiv').mouseenter(function() {
+		$(this).addClass('filled');
+	});
+
+	//make a cool grid
+	var newGrid = function(n){
+		for (var i=0; i<n; i++) {
 			//jQuery scrubs such as I must build from scratch
-			$gB.append('<div class="gridRow" id="gR'+i+'"></div>')
-			for (var j=0; j<r; j++) {
+			gB.append('<div class="gridRow" id="gR'+i+'"></div>')
+			for (var j=0; j<n; j++) {
 				$('#gR'+i).append('<div class="gridDiv"></div>');
 			};
 		};
 	}
-	newGrid(16,16)
-	
-	$('.gridDiv').mouseenter(function() {
-		$(this).addClass('filled');
+	newGrid(16);
+
+	//shake the etch-a-sketch
+	$('button').click(function() {
+		gB.empty();
+		var n = prompt("How many square pixels?");
+		newGrid(n);
 	});
+
 });
